9004416207b08896062fdd5102324ddd73a7c901
Kayzer <max.mlv2210@gmail.com>
2023-12-10 20:34:00 +0100

    Les types de données

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 let MaVariable = "Hello";
 console.log(MaVariable);
 
+// *********************************** LES VARIABLES *********************************** //
 
 // Variable = Une boite, avec des donné dedans.
 // Avant on utilisait "var" pour nomé sa variable. var = vieux JavaScript.
@@ -25,6 +26,12 @@ let unChiffre = 24;
 unChiffre = 22;
 console.log(unChiffre);
 
+// Variable seul
+// Ex : Une variable qui peut etre remplis par l'utilisateur avec un input
+let arbre;
+arbre = "sapin";
+
+// *********************************** LES GUILLEMETS *********************************** //
 
 // En JavaScript qu'on utilise les guillemet ("") ou ('') sa ne change rien.
 let chaine = "Je suis une chaine de caractères";
@@ -35,3 +42,21 @@ let autreConcatenation = `Chaine précédente : ${chaine}. Voila c'était le con
 console.log(nouvelleChaine);
 console.log(autreConcatenation);
 
+// *********************************** LES DONNEES *********************************** //
+// Types de données.
+let string = "Je suis une chaine de carractère";
+let number = 24;
+let boolean = true/false;
+
+// Tableau il y a des crochets [], et je peut y mettre des "string", des "number", des "boolean".
+// Entre les élément on met une (,) et pas un (;)
+let array = ["je", "suis", "un", "tableau"]
+
+// Objet il y a des accolades {}, chaque élément de l'objet à une clé et une valeur
+// Ex: La clé = "prenom". La valeur = "Kayzer".
+// Entre les élément on met une (,) et pas un (;)
+let object = {
+    prenom: "kayzer",
+    age: 33,
+    ville: "bordeaux",
+}

7cb0c374e79a7ed7638ec81c51072b0b1e0776e0
Kayzer <max.mlv2210@gmail.com>
2023-12-10 19:00:43 +0100

    Première ligne JS

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Salut JS");
+console.log("");
+
+// Ecrir "log +  select log to the console" pour avoir un "console.log ("");"
+
+// Toujours mettre un " ; " a chaque fin de ligne. Même si cela n'est pas obligatoire c'est une bonne pratique.
